c20d7de84030fe9d28f68f654e6e2ccc80c4e503
Computer Engineering Student Association <connect.cesaofficial@gmail.com>
2026-01-12 10:44:15 +0530

    Update Events.jsx

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -70,7 +70,7 @@ const Events = () => {
         transition={{ duration: 0.5 }}
       >
         <div className="max-w-sm mx-auto mb-8">
-            <Navbar />
+           
           <img
             src={image}
             alt="No events"
@@ -110,7 +110,7 @@ const Events = () => {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-white to-gray-50 dark:from-gray-900 dark:to-gray-800 py-16 px-4 sm:px-6 lg:px-8 transition-colors duration-200">
-     
+      <Navbar />
       <div className="max-w-7xl mx-auto">
         {/* Grid background */}
         <div className="absolute inset-0 opacity-25">
